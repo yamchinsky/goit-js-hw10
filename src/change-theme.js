3a7eb11e7eb417b20f1f2ba59dbd510413af8e69
yamchinsky <66372922+yamchinsky@users.noreply.github.com>
2021-05-13 18:18:22 +0300

    changes

--- src/change-theme.js
+++ src/change-theme.js
@@ -12,15 +12,20 @@ const Theme = {
 
 export function changeTheme(e) {
   if (e.target.checked) {
-        body.classList.add(Theme.DARK)
-        body.classList.remove(Theme.LIGHT)
-        localStorage.setItem('theme', 'dark');
+    updateClass(Theme.DARK, Theme.LIGHT)
+    localStorage.setItem('theme', 'dark');
     }
     else {
-        body.classList.add(Theme.LIGHT)
-         body.classList.remove(Theme.DARK)
-         localStorage.setItem('theme', 'light');
+    updateClass(Theme.LIGHT, Theme.DARK)
+      localStorage.setItem('theme', 'light');
     }
 }
+
+
+function updateClass(addClass, removeClass) {
+body.classList.add(addClass)
+body.classList.remove(removeClass)
+
+};
 
 
